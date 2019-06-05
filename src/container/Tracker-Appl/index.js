65b5296bd83002507a5661ac76d68714a1b7e9fc
SiimonStark <justinpyktel@gmail.com>
2019-06-04 20:33:03 -0600

    Working Tracker

--- src/container/Tracker-Appl/index.js
+++ src/container/Tracker-Appl/index.js
@@ -3,43 +3,40 @@ import {connect} from 'react-redux';
 import {counterAppl} from '../../store/actions/';
 
 export class TrackerAppl extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      counter: this.props.counterAppl
-    }
-
-  }
 
   handleClick = (e) => {
-    let {value} = e.target;
-    let {counter} = this.state;
-    value === '+' ? counter ++ : counter --;
+    let {id} = e.target;
+    let {counter} = this.props
 
-    counterAppl(counter);
-    this.setState({counter});
+    if (counter > 0) {
+      id === 'plus' ? counter ++ : counter --;
+    } else {
+      id === 'plus' ? counter++ : counter = 0;
+    }
+
+    this.props.counterAppl(counter);
   }
 
   render() {
     return (
       <section className="Tracker Applied">
         <h3>Jobs Applied</h3>
-        <article>
-          <h2>{this.state.counter}</h2>
-          <button onClick={this.handleClick}>
-            <i class="fas fa-minus-circle"></i>
-          </button>
-          <button onClick={this.handleClick}>
-            <i className="fas fa-plus-circle"></i>
-          </button>
-        </article>
+        <h2>{this.props.counter}</h2>
+        <div className="Tracker__btn">
+          <i id="minus"
+            onClick={this.handleClick}
+            className="fas fa-minus-circle"></i>
+          <i id="plus"
+            onClick={this.handleClick}
+            className="fas fa-plus-circle"></i>
+        </div>
       </section>
     )
   }
 }
 
 export const mapStateToProps = (state) => ({
-  counterAppl: state.counterAppl
+  counter: state.counterAppl
 });
 
 export const mapDispatchToProps = (dispatch) => ({
